f49b42515861860ec542ca649835b3fad4f8cca7
Kyle Larose <eomereadig@gmail.com>
2017-08-27 10:27:17 -0400

    Add socket connection
    This commit adds a rudimentary example of connecting to the provided URI
    by:
     1) Retrieving the address from the host (hardcoded port 80 for now)
     2) Opening a socket
     3) connecting to the address from #1.
    
    Note:
     * We do not yet parse the port from the uri
     * We're assuming http/port 80.
     * We only use the first result from getaddrinfo for simplicity

--- goget.cpp
+++ goget.cpp
@@ -4,6 +4,11 @@
 #include <boost/program_options.hpp>
 #include <boost/network/uri.hpp>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
 namespace po = boost::program_options;
 
 CmdLineArgs GetArgsFromCmdLine(int argc, char** argv)
@@ -45,6 +50,46 @@ CmdLineArgs GetArgsFromCmdLine(int argc, char** argv)
     return args;
 }
 
+bool ConnectToURI(const boost::network::uri::uri& uri)
+{
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;          /* Any protocol */
+
+	struct addrinfo *result = nullptr;
+	int ret = getaddrinfo(uri.host().c_str(), "80", &hints, &result);
+
+	if (ret != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+        return false;
+	}
+
+    int socketFd = socket(result->ai_family,
+                          result->ai_socktype,
+                          result->ai_protocol);
+
+    if(socketFd < 0)
+    {
+        perror("socket");
+        return false;
+    }
+    
+    int connectResult = connect(socketFd,
+                                result->ai_addr,
+                                result->ai_addrlen);
+    if(connectResult != 0)
+    {
+        perror("connect");
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     CmdLineArgs args = GetArgsFromCmdLine(argc, argv);
@@ -60,6 +105,16 @@ int main(int argc, char** argv)
             toGet.path().c_str(),
             toGet.host().c_str(),
             args.outFile.c_str());
+
+    bool connected = ConnectToURI(toGet);
+    if(connected)
+    {
+        printf("Successfully connected\n");
+    }
+    else
+    {
+        printf("Failed to connect\n");
+    }
 
     return 0;
 }
